282258e3059599636d34c1c866beea8a2f5768b5
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-05-28 12:40:29 +0900

    20230528 First Commit

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val jdkVersion: String by project
+val arrowVersion: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    id("io.ktor.plugin") version "2.3.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21"
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(jdkVersion))
+    }
+}
+
+group = "net.konohana.sakuya.cerinthe"
+version = "0.0.1"
+application {
+    mainClass.set("io.ktor.server.jetty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-jackson-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-cors-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-host-common-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-jetty-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+
+    //kotlinx-datetime
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+    // Arrow-kt
+    implementation("io.arrow-kt:arrow-core:$arrowVersion")
+}
+
+// 'gradle jar'を使えるようにタスクを定義
+val jar by tasks.getting(Jar::class) {
+    duplicatesStrategy= DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = "net.konohana.sakuya.cerinthe.ApplicationKt"
+    }
+
+    from(
+        configurations.runtimeClasspath.get().map {
+            if (it.isDirectory) it else zipTree(it)
+        }
+    )
+    exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
+}
---
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/Application.kt
@@ -0,0 +1,17 @@
+package net.konohana.sakuya.cerinthe
+
+import io.ktor.server.application.Application
+import net.konohana.sakuya.cerinthe.plugins.configureHTTP
+import net.konohana.sakuya.cerinthe.plugins.configureRouting
+import net.konohana.sakuya.cerinthe.plugins.configureSerialization
+
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.jetty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureHTTP()
+    configureRouting()
+}
