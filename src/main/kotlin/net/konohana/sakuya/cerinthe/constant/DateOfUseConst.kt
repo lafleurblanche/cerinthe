c65aeffbc734bbd9e7720f9bd69a534ac17a2d37
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-05-28 18:39:39 +0900

    20230528 #3  利用日

---
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/constant/DateOfUseConst.kt
@@ -0,0 +1,15 @@
+package net.konohana.sakuya.cerinthe.constant
+
+/**
+ * ## 利用日定数クラス
+ * @author lafleurblanche
+ */
+class DateOfUseConst {
+    companion object {
+        const val DATE_NOT_SET = "日付未指定"
+        const val  DATE_SET_TODAY = "本日"
+        const val  DATE_SET_TOMORROW = "明日"
+        const val  DATE_SET_DAY_AFTER_TOMORROW = "明後日"
+        const val  DATE_SET_ONE_MONTH = "1ヵ月後"
+    }
+}
