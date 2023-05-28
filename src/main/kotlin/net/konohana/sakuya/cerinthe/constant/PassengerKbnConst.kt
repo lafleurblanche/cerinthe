237342d86b3336e3552c5f1d97a2980d870681a9
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-05-28 20:11:35 +0900

    20230528 #7 旅客区分

---
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/constant/PassengerKbnConst.kt
@@ -0,0 +1,12 @@
+package net.konohana.sakuya.cerinthe.constant
+
+class PassengerKbnConst  {
+    companion object {
+        /** 旅客区分: 大人 */
+        const val PASSENGER_KBN_ADULT = "adult/"
+        /** 旅客区分: 小児 */
+        const val PASSENGER_KBN_CHILD = "child/"
+        /** 旅客区分: 半額 */
+        const val PASSENGER_KBN_HALF = "half/"
+    }
+}
