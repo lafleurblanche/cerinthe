bfd3b6778ae9ee4412b6268c80ff0c8517010255
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-05-28 18:41:30 +0900

    20230528 #5 乗車人員初期値

---
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/constant/MemberConst.kt
@@ -0,0 +1,13 @@
+package net.konohana.sakuya.cerinthe.constant
+
+/**
+ * 乗車人員定数クラス
+ */
+class MemberConst {
+    companion object {
+        /** 乗車人員0名 */
+        const val MEMBER_NONE = 0
+        /** 乗車人員1名 */
+        const val MEMBER_MIN = 1
+    }
+}
