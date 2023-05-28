d89acebfcced888a05f3fc2b4304b189d2842815
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-05-28 22:02:08 +0900

    20230528 #8 経路コード(AreaGW)

---
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/constant/RouteCodeConst.kt
@@ -0,0 +1,12 @@
+package net.konohana.sakuya.cerinthe.constant
+
+/**
+ * ## 経路コード定数クラス
+ * * AreaGW
+ * @author lafleurblanche
+ */
+class RouteCodeAreaGWConst {
+    companion object {
+        const val ROUTE_CODE_101 = "101"
+    }
+}
