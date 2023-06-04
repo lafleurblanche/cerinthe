007487591d6c686937212dfedd7803a3e6af9d6e
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-04 21:01:17 +0900

    20230604 #12 券面種別(運賃計算用)

---
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/constant/TicketTypeCalcConst.kt
@@ -0,0 +1,11 @@
+package net.konohana.sakuya.cerinthe.constant
+
+/**
+ * 券面種別(運賃計算用)定数クラス
+ */
+class TicketTypeCalcConst {
+    companion object {
+        const val CALC_TICKET_TYPE_ONEWAY = "片道乗車券"
+        const val CALC_TICKET_TYPE_ROUNDTRIP = "往復乗車券"
+    }
+}
