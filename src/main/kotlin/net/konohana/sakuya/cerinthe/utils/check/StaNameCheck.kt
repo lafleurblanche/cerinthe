998130d01f59ff7dac78a3f45e49325f4375c287
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-12 11:57:26 +0900

    20230612 新規作成

---
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/utils/check/StaNameCheck.kt
@@ -0,0 +1,18 @@
+package net.konohana.sakuya.cerinthe.utils.check
+
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import net.konohana.sakuya.cerinthe.constant.CheckValueConst
+import net.konohana.sakuya.cerinthe.utils.result.ResultError
+import net.konohana.sakuya.cerinthe.utils.result.ResultNormal
+
+fun staNameCheck(from: String, to: String): Either<ResultError, ResultNormal> {
+    return if (
+        from == CheckValueConst.CHECK_VALUE_FROMSTA_DEFAULT ||
+        to == CheckValueConst.CHECK_VALUE_TOSTA_DEFAULT) ResultError.StaNameNotSet.left()
+    else if ( from == to) {
+        ResultError.StaNameDuplicate.left()
+    }
+    else ResultNormal.CheckSuccess.right()
+}
