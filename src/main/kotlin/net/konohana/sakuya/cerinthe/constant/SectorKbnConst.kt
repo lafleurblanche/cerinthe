a40fdefcfaeabb8f6363a387b241a7bd5385f3de
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-15 22:34:13 +0900

    20230615 #17 新規作成

---
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/constant/SectorKbnConst.kt
@@ -0,0 +1,12 @@
+package net.konohana.sakuya.cerinthe.constant
+
+/**
+ * 系統会社区分定数クラス
+ * @author lafleurblanche
+ */
+class SectorKbnConst {
+    companion object {
+        const val SECTOR_KBN_AREAGW = "ARGW"
+        const val SECTOR_KBN_ENJURW = "ENJU"
+    }
+}
