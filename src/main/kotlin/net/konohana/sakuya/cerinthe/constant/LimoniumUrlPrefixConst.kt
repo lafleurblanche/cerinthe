ce926c8c026d562701b06f86650515d3d0c58f76
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-17 19:40:43 +0900

    20230617 #13 新規作成

---
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/constant/LimoniumUrlPrefixConst.kt
@@ -0,0 +1,14 @@
+package net.konohana.sakuya.cerinthe.constant
+
+class LimoniumUrlPrefixConst {
+    companion object {
+        const val ARGW_ONEWAY_ADULT = "regist/argw-req-owa"
+        const val ARGW_ROUNDTRIP_ADULT = "regist/argw-req-rta"
+        const val ARGW_ONEWAY_HALF = "regist/argw-req-owh"
+        const val ARGW_ROUNDTRIP_HALF = "regist/argw-req-rth"
+        const val ENJU_ONEWAY_ADULT = "regist/enju-req-owa"
+        const val ENJU_ROUNDTRIP_ADULT = "regist/enju-req-rta"
+        const val ENJU_ONEWAY_HALF = "regist/enju-req-owh"
+        const val ENJU_ROUNDTRIP_HALF = "regist/enju-req-rth"
+    }
+}
