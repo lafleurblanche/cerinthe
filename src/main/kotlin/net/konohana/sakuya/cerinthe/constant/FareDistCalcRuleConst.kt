8f5d5f0cc9282d448eb5dfb046a61272923dc6d9
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-20 01:46:51 +0900

    20230620 Add

--- src/main/kotlin/net/konohana/sakuya/cerinthe/constant/FareDistCalcRuleConst.kt
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/constant/FareDistCalcRuleConst.kt
@@ -10,5 +10,21 @@ class FareDistCalcRuleConst {
         const val FAREDIST_CALCRULE_PATTERN_A = "A"
         /** 経路情報計算ルール(発駅＋着駅) */
         const val FAREDIST_CALCRULE_PATTERN_B = "B"
+        /** 経路情報計算ルール(中間駅(苆谷)) */
+        const val FAREDIST_CALCRULE_PATTERN_C1 = "C1"
+        /** 経路情報計算ルール(中間経路(苆谷～茄谷)) */
+        const val FAREDIST_CALCRULE_PATTERN_D1 = "D1"
+        /** 経路情報計算ルール(中間経路(苆谷～笥谷)) */
+        const val FAREDIST_CALCRULE_PATTERN_D2 = "D2"
+        /** 経路情報計算ルール(中間経路(苆谷～筏谷)) */
+        const val FAREDIST_CALCRULE_PATTERN_D3 = "D3"
+        /** 経路情報計算ルール(中間経路(苆谷～筮谷)) */
+        const val FAREDIST_CALCRULE_PATTERN_D4 = "D4"
+        /** 経路情報計算ルール(中間経路(苆谷～槐谷)) */
+        const val FAREDIST_CALCRULE_PATTERN_D5 = "D5"
+        /** 経路情報計算ルール(中間経路(苆谷～卯塚)) */
+        const val FAREDIST_CALCRULE_PATTERN_D6 = "D6"
+        /** 経路情報計算ルール(中間経路(苆谷～瑞堂)) */
+        const val FAREDIST_CALCRULE_PATTERN_D7 = "D7"
     }
 }
