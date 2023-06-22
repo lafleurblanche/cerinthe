07b1aa40658611b535f236183b3dbe4eab178203
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-22 21:06:08 +0900

    20230622 新規作成

---
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/utils/calcdist/enjurw/ptnc/FareDistCalcPatternC8.kt
@@ -0,0 +1,21 @@
+package net.konohana.sakuya.cerinthe.utils.calcdist.enjurw.ptnc
+
+import net.konohana.sakuya.cerinthe.constant.RouteDistanceEnjuRWConst
+import kotlin.math.abs
+import kotlin.math.ceil
+
+/**
+ * ## 距離計算処理
+ * * えんじゅ交通
+ * * 距離情報計算ルール: パターンC8
+ * * 発駅と着駅を合算した距離情報を返却します.
+ * * 中間区間(笒谷)を経路として含む場合.
+ * @param fromDist 発駅距離情報
+ * @param toDist 着駅距離情報
+ * @return 営業キロ, 運賃計算キロ
+ */
+fun faredistCalcPatternC8(fromDist: Double, toDist: Double): Pair<Double, Int> {
+    val dist = abs(fromDist - RouteDistanceEnjuRWConst.ROUTE_DISTANCE_ENJU_PTN_C8) + toDist
+    val calcDist = ceil(dist * 10.0 / 10.0).toInt()
+    return Pair(dist, calcDist)
+}
