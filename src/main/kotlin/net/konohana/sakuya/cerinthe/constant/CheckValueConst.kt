ef3a3882c2b46b2921cfc31cc3863f60ee349c11
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-25 01:25:47 +0900

    20230625 定数追加

--- src/main/kotlin/net/konohana/sakuya/cerinthe/constant/CheckValueConst.kt
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/constant/CheckValueConst.kt
@@ -10,5 +10,9 @@ class CheckValueConst {
         const val CHECK_VALUE_TOSTA_DEFAULT = "降車駅"
         const val CHECK_VALUE_DAYOFUSE_DEFAULT = "乗車日"
         const val CHECK_VALUE_MEMBER_DEFAULT = "0"
+        const val CHECK_VALUE_FROMSTA_CODE_DEFAULT = "乗車駅コード"
+        const val CHECK_VALUE_TOSTA_CODE_DEFAULT = "降車駅コード"
+        const val CHECK_VALUE_MONTH_DEFAULT = "乗車月"
+        const val CHECK_VALUE_TICKET_TYPE_DEFAULT = "券種別"
     }
 }
