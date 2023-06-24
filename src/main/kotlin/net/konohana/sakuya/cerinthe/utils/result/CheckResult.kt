73e278e3a52318492f5ee52e50e5828dfa02ec82
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-25 01:26:26 +0900

    20230625 エラー結果追加

--- src/main/kotlin/net/konohana/sakuya/cerinthe/utils/result/CheckResult.kt
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/utils/result/CheckResult.kt
@@ -2,9 +2,12 @@ package net.konohana.sakuya.cerinthe.utils.result
 
 open class ResultError {
     object NotFound : ResultError()
+    object StaCodeDuplicate : ResultError()
+    object StaCodeNotSet : ResultError()
     object StaNameDuplicate : ResultError()
     object StaNameNotSet : ResultError()
     object DayOfUseNotSet : ResultError()
+    object MonthNotSet : ResultError()
     object MemberNotSet : ResultError()
 
     object SectorNotApplicable : ResultError()
