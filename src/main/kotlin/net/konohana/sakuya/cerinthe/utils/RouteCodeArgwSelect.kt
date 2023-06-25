ec1617bf2865f70d83a09e4e65c3f32c60836469
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-26 04:07:25 +0900

    20230626 #53 路線コード誤り修正

--- src/main/kotlin/net/konohana/sakuya/cerinthe/utils/RouteCodeArgwSelect.kt
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/utils/RouteCodeArgwSelect.kt
@@ -12,7 +12,7 @@ fun routeCodeArgwSelect(from: String, to: String): String {
     val fromRoute = from.substring(IndexConst.INDEX_START, IndexConst.INDEX_END)
     val toRoute = to.substring(IndexConst.INDEX_START, IndexConst.INDEX_END)
     val routeMap = mapOf(
-        Pair("RW01", "RW01") to RouteCodeAreaGWConst.ROUTE_CODE_101,
+        Pair("GW01", "GW01") to RouteCodeAreaGWConst.ROUTE_CODE_101,
     )
     return routeMap.getOrDefault(Pair(fromRoute, toRoute), "経路情報誤り")
 }
