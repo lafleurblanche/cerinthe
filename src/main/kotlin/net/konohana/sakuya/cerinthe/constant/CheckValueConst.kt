651cbf90319fd23d4c3be825b97d2b7a87bcf6f5
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-26 04:49:28 +0900

    20230626 定数追加

--- src/main/kotlin/net/konohana/sakuya/cerinthe/constant/CheckValueConst.kt
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/constant/CheckValueConst.kt
@@ -8,6 +8,7 @@ class CheckValueConst {
     companion object {
         const val CHECK_VALUE_FROMSTA_DEFAULT = "乗車駅"
         const val CHECK_VALUE_TOSTA_DEFAULT = "降車駅"
+        const val CHECK_VALUE_STATION_UNDEFINED = "該当駅なし"
         const val CHECK_VALUE_DAYOFUSE_DEFAULT = "乗車日"
         const val CHECK_VALUE_MEMBER_DEFAULT = "0"
         const val CHECK_VALUE_FROMSTA_CODE_DEFAULT = "乗車駅コード"
