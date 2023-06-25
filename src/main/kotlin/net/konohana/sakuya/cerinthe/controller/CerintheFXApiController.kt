f465d43a9aceb0ae27ffaaf7244f885544422265
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-26 04:49:51 +0900

    20230626 判定処理追加

--- src/main/kotlin/net/konohana/sakuya/cerinthe/controller/CerintheFXApiController.kt
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/controller/CerintheFXApiController.kt
@@ -1,16 +1,27 @@
 package net.konohana.sakuya.cerinthe.controller
 
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
+import kotlinx.serialization.json.Json
 import net.konohana.sakuya.cerinthe.dto.request.CerintheFXRequestData
 import net.konohana.sakuya.cerinthe.dto.response.CerintheAPIResponse
+import net.konohana.sakuya.cerinthe.dto.response.PhaceliaFareDistResponse
+import net.konohana.sakuya.cerinthe.utils.apiUrlArgwJudge
+import net.konohana.sakuya.cerinthe.utils.calcDist
 import net.konohana.sakuya.cerinthe.utils.check.requestDataFXCheck
 import net.konohana.sakuya.cerinthe.utils.fareDistCalcRuleJudgement
+import net.konohana.sakuya.cerinthe.utils.fxApiUrlEnjuJudge
 import net.konohana.sakuya.cerinthe.utils.fxRouteCodeEnjuRWSelect
 import net.konohana.sakuya.cerinthe.utils.fxRouteInfoEnjuRWSelect
 import net.konohana.sakuya.cerinthe.utils.passengerGroupJudge
@@ -35,9 +46,29 @@ fun Route.cerintheFXApiController() {
             println("経路情報${routeInfo}")
             // 経路情報コード判定
             val routeCode: String = fxRouteCodeEnjuRWSelect(from, to)
+            // APIURL判定処理
+            val url = fxApiUrlEnjuJudge(from, to)
             // 距離計算ルール判定
             val calcPattern = fareDistCalcRuleJudgement(routeCode)
             println(calcPattern)
+            // API呼び出し
+            val client = HttpClient(CIO) {
+                install(ContentNegotiation) {
+                    json(Json {
+                        prettyPrint = true
+                        isLenient = true
+                    })
+                }
+            }
+            // 距離情報取得APIから距離情報を取得
+            val fromResponse: PhaceliaFareDistResponse = client.get(url.first).body()
+            val toResponse: PhaceliaFareDistResponse = client.get(url.second).body()
+            val fromDist = fromResponse.distance
+            val toDist = toResponse.distance
+            // 営業キロ, 運賃計算キロを算出
+            val dist = calcDist(calcPattern, fromDist, toDist)
+            println("営業距離${dist.first}")
+            println("運賃計算距離${dist.second}")
             call.respond(
                 HttpStatusCode.OK,
                 CerintheAPIResponse(
