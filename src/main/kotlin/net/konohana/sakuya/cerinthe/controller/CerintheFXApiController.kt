f55bff469344738abeb8fb2ca1a32625319d6de5
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-26 01:12:44 +0900

    20230625 判定処理追加

--- src/main/kotlin/net/konohana/sakuya/cerinthe/controller/CerintheFXApiController.kt
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/controller/CerintheFXApiController.kt
@@ -10,13 +10,34 @@ import io.ktor.server.routing.route
 import net.konohana.sakuya.cerinthe.dto.request.CerintheFXRequestData
 import net.konohana.sakuya.cerinthe.dto.response.CerintheAPIResponse
 import net.konohana.sakuya.cerinthe.utils.check.requestDataFXCheck
+import net.konohana.sakuya.cerinthe.utils.fareDistCalcRuleJudgement
+import net.konohana.sakuya.cerinthe.utils.fxRouteCodeEnjuRWSelect
+import net.konohana.sakuya.cerinthe.utils.fxRouteInfoEnjuRWSelect
+import net.konohana.sakuya.cerinthe.utils.passengerGroupJudge
 
 fun Route.cerintheFXApiController() {
     route("/cerinthe/fx/api/regist-ticket") {
         post {
             val req = call.receive<CerintheFXRequestData>()
             println(req)
+            val from = req.fromStaCode
+            val to = req.toStaCode
             requestDataFXCheck(req)
+            // 旅客グループ判定
+            val passengerGrp = passengerGroupJudge(
+                req.adultMember,
+                req.halfMember,
+                req.childMember
+            )
+            println(passengerGrp)
+            // 経路情報判定
+            val routeInfo: String = fxRouteInfoEnjuRWSelect(from, to)
+            println("経路情報${routeInfo}")
+            // 経路情報コード判定
+            val routeCode: String = fxRouteCodeEnjuRWSelect(from, to)
+            // 距離計算ルール判定
+            val calcPattern = fareDistCalcRuleJudgement(routeCode)
+            println(calcPattern)
             call.respond(
                 HttpStatusCode.OK,
                 CerintheAPIResponse(
