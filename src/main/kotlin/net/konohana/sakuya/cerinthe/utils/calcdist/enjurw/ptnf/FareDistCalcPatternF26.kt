3e0d8075266b3bf53c2a906e5a05380eb0612726
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-26 18:33:12 +0900

    20230626 新規作成

---
+++ src/main/kotlin/net/konohana/sakuya/cerinthe/utils/calcdist/enjurw/ptnf/FareDistCalcPatternF26.kt
@@ -0,0 +1,27 @@
+package net.konohana.sakuya.cerinthe.utils.calcdist.enjurw.ptnf
+
+import net.konohana.sakuya.cerinthe.constant.RouteDistanceEnjuRWConst
+import kotlin.math.ceil
+
+/**
+ * ## 距離計算処理
+ * * えんじゅ交通
+ * * 距離情報計算ルール: パターンF26
+ * * 発駅と着駅を合算した距離情報を返却します.
+ * * 中間区間#1(扆谷~瑞堂)を経路として含む場合.
+ * * 中間区間#2(瑞堂~卯塚)を経路として含む場合.
+ * * 中間区間#3(卯塚~槐谷)を経路として含む場合.
+ * @param fromDist 発駅距離情報
+ * @param toDist 着駅距離情報
+ * @return 営業キロ, 運賃計算キロ
+ */
+fun faredistCalcPatternF26(fromDist: Double, toDist: Double): Pair<Double, Int> {
+    val dist =
+        fromDist +
+        RouteDistanceEnjuRWConst.ROUTE_DISTANCE_ENJU_RW19_PTN_CMN02 +
+        RouteDistanceEnjuRWConst.ROUTE_DISTANCE_ENJU_RW17_PTN_CMN06 +
+        RouteDistanceEnjuRWConst.ROUTE_DISTANCE_ENJU_RW17_PTN_CMN01 +
+        toDist
+    val calcDist = ceil(dist * 10.0 / 10.0).toInt()
+    return Pair(dist, calcDist)
+}
